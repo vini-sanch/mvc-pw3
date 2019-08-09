729c8697b475f0c9aa2f7d8d4276772ba31762b2
Vinícius <vi.santachiara14@gmail.com>
2019-08-09 19:23:39 -0300

    Subindo o arquivo ddl do banco de dados

---
+++ database/atividade_pw3.sql
@@ -0,0 +1,59 @@
+#Cria o banco de dados
+CREATE DATABASE `JORNAL`;
+#seleciona o banco
+USE `JORNAL`;
+
+ALTER DATABASE `JORNAL` CHARSET = UTF8 COLLATE = utf8_unicode_ci;
+
+#cria a tabela Usuário
+CREATE TABLE `USUARIO`(
+	#colunas
+	`CODUSUARIO`	INT AUTO_INCREMENT,
+    `NOME` 			VARCHAR(50)  NOT NULL,
+    `EMAIL`			VARCHAR(50)  NOT NULL,
+    `SENHA`			VARCHAR(100) NOT NULL,
+    `NIVEL_ACESSO`	VARCHAR(20)  NOT NULL,
+    #chave primária
+    PRIMARY KEY (`CODUSUARIO`)
+);
+
+#cria a tabela Categoria
+CREATE TABLE `CATEGORIA` (
+	`CODCATEGORIA`		INT AUTO_INCREMENT,
+    `NOMECATEGORIA` 	VARCHAR(20),
+    
+    PRIMARY KEY (`CODCATEGORIA`)
+);
+
+#cria a tabela notícia
+CREATE TABLE `NOTICIA` (
+	#atributos
+	`CODNOTICIA`	INT AUTO_INCREMENT,
+    `TITULO`		VARCHAR(40) NOT NULL,
+    `CONTEUDO`		TEXT,
+    `DATA`			DATE NOT NULL,
+    `AUTOR`			VARCHAR(50) NOT NULL,
+    `IMAGEM`		VARCHAR(60),
+    `CODCATEGORIA`	INT,
+    
+    #chave primária
+    PRIMARY KEY (`CODNOTICIA`),
+    
+    #chave estrangeira
+    CONSTRAINT `FK_CATEGORIA_NOTICIA`
+		FOREIGN KEY (`CODCATEGORIA`)
+        REFERENCES `CATEGORIA` (`CODCATEGORIA`)
+);
+
+/* Criação de indexações para as tabelas */
+
+# indexando pelo nome (tabela usuário)
+CREATE INDEX IDX_NOME ON `USUARIO` (`NOME`);
+# indíce único para e-mail (chave candidata tabela usuário)
+CREATE UNIQUE INDEX IDX_EMAIL ON `USUARIO` (`EMAIL`);
+
+#indexando pelo nome da categoria (tabela categoria)
+CREATE INDEX IDX_NOMECATEGORIA ON `CATEGORIA` (`NOMECATEGORIA`);
+
+# indexando pela data, título e autor (tabela notícia)
+CREATE INDEX IDX_NOTICIA ON `NOTICIA` (`DATA`, `TITULO`, `AUTOR`);
