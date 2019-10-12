1fd7145168ebf0bf9e71123afe4d37d443a4f3eb
Vinícius <vi.santachiara14@gmail.com>
2019-10-11 22:27:36 -0300

    Novos dados

--- database/bdnoticia.sql
+++ database/bdnoticia.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: 20-Set-2019 às 23:57
+-- Generation Time: 12-Out-2019 às 03:26
 -- Versão do servidor: 10.1.19-MariaDB
 -- PHP Version: 7.0.13
 
@@ -41,6 +41,8 @@ CREATE TABLE `categoria` (
 INSERT INTO `categoria` (`CODCATEGORIA`, `NOMECATEGORIA`) VALUES
 (3, 'Cultura'),
 (1, 'Esportes'),
+(6, 'Mulher'),
+(5, 'Natureza'),
 (2, 'PolÃ­tica'),
 (4, 'ViolÃªncia');
 
@@ -66,7 +68,12 @@ CREATE TABLE `noticia` (
 --
 
 INSERT INTO `noticia` (`CODNOTICIA`, `TITULO`, `CONTEUDO`, `DATA`, `AUTOR`, `IMAGEM`, `CODCATEGORIA`) VALUES
-(1, 'Minha NotÃ­cia', 'hb  gjtj j rtj fgj t jfgj tfg jf gjdfgjfd u5rtyk jsgnm \\dh nxr jnt,.;i;.Ã§l.lul,', '2019-09-06', 'Eu mesmo', 'Wallpaper-EFM.jpg', 2);
+(1, 'Minha NotÃ­cia', 'hb  gjtj j rtj fgj t jfgj tfg jf gjdfgjfd u5rtyk jsgnm \\dh nxr jnt,.;i;.Ã§l.lul,', '2019-09-06', 'Eu mesmo', 'Wallpaper-EFM.jpg', 2),
+(2, 'Ãrvore', 'As Ã¡rvores somos noses', '2019-10-10', 'Jesus', 'platano-arvore.jpg', 5),
+(3, 'Chicletes Assassinos', 'Cuidado com os doces que vocÃª dÃ¡ o seu filhos, esta semana encontramos alguns chicletes envenenados', '2019-10-01', 'Jack', 'c360_2018-12-03-14-33-04-802.jpg', 4),
+(4, 'Escolha um Lado', 'NÃ£o vou te dar conselhos', '2019-10-07', 'Indeciso', '3-dicas-para-fazer-a-politica-da-qualidade-valer-a-pena.png', 2),
+(5, 'Qual Ã‰ A Hora Certa Para Ter BebÃªs?', 'A hora que vocÃª quiser, sÃ³ cuidado com a menopausa', '2019-11-29', 'Senhor Ginecologista', 'bebe-prematuro-cuidados-656x370.jpg', 6),
+(6, 'Sumidos No Mar', 'Onde ele estarÃ¡?', '2019-08-04', 'Desconhecido', 'ritual-ma.png', 4);
 
 -- --------------------------------------------------------
 
@@ -126,12 +133,12 @@ ALTER TABLE `usuario`
 -- AUTO_INCREMENT for table `categoria`
 --
 ALTER TABLE `categoria`
-  MODIFY `CODCATEGORIA` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `CODCATEGORIA` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 --
 -- AUTO_INCREMENT for table `noticia`
 --
 ALTER TABLE `noticia`
-  MODIFY `CODNOTICIA` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `CODNOTICIA` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 --
 -- AUTO_INCREMENT for table `usuario`
 --
